d306b0113ba83c3d328d379de2214ef6f3619269
Ray Donnelly <mingw.android@gmail.com>
2017-10-04 14:25:01 +0100

    Add cross-compilation support
    Have removed openjpeg for now. Not sure why this needs two different
    jpeg libraries?

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,13 +4,17 @@ set -e
 
 # The zlib check does not let you specify its install prefix so we have
 # to go global.
+<<<<<<< HEAD
 export CFLAGS="${CFLAGS} -I${PREFIX}/include"
+=======
+>>>>>>> Add cross-compilation support
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 if [[ ${HOST} =~ .*darwin.* ]] ; then
     export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
 fi
 
+<<<<<<< HEAD
 mkdir build && cd build
 
 cmake -G "$CMAKE_GENERATOR" \
@@ -25,6 +29,23 @@ cmake -G "$CMAKE_GENERATOR" \
 make -j$CPU_COUNT
 # ctest  # no tests were found :-/
 make install -j$CPU_COUNT
+=======
+configure_args=(
+    --prefix=${PREFIX}
+    --disable-dependency-tracking
+    --enable-xpdf-headers
+    --enable-libcurl
+    --enable-introspection=auto
+    --disable-gtk-doc
+    --disable-gtk-test
+)
+
+
+./configure "${configure_args[@]}" || { cat config.log ; exit 1 ; }
+make -j${CPU_COUNT} ${VERBOSE_AT}
+# make check requires a big data download
+make install
+>>>>>>> Add cross-compilation support
 
 pushd ${PREFIX}
   rm -rf lib/libpoppler*.la lib/libpoppler*.a share/gtk-doc
