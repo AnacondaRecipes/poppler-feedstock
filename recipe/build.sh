a092f510acb420b43835f796809aa4db317510ae
Michael Sarahan <msarahan@gmail.com>
2017-10-09 12:01:08 -0500

    update to 60.1, switch to cmake instead of pkgconfig/autotools

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,25 +4,25 @@ set -e
 
 # The zlib check does not let you specify its install prefix so we have
 # to go global.
+export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 if [[ ${HOST} =~ .*darwin.* ]] ; then
     export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
 fi
 
-configure_args=(
-    --prefix=${PREFIX}
-    --disable-dependency-tracking
-    --enable-xpdf-headers
-    --enable-libcurl
-    --enable-introspection=auto
-    --disable-gtk-doc
-    --disable-gtk-test
-)
+mkdir build
+cd build
 
+cmake -G "$CMAKE_GENERATOR" \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DENABLE_XPDF_HEADERS=True \
+      -DENABLE_LIBCURL=True \
+      -DENABLE_LIBOPENJPEG=openjpeg2 \
+      ..
 
-./configure "${configure_args[@]}" || { cat config.log ; exit 1 ; }
-make -j${CPU_COUNT} ${VERBOSE_AT}
+make -j${CPU_COUNT}
 # make check requires a big data download
 make install
 
